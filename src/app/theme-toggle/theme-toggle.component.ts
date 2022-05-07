1109761ab8db97a76756f20743503ffdc95d1be3
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-06 18:53:50 -0600

    📅  | Fri May  6 18:53:50 MDT 2022

--- src/app/theme-toggle/theme-toggle.component.ts
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.soss'],
+  styleUrls: ['./theme-toggle.component.scss'],
 })
 export class ThemeToggleComponent implements OnInit {
   constructor() {}
-
+  isDark = false;
   ngOnInit(): void {}
-  toggleTheme(theme: any) {}
+  ngAfterContentChecked() {}
+  changeTheme(event: any) {
+    event.stopPropagation();
+    console.log(this.isDark);
+  }
 }
