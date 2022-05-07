d4fc13771632af800ce996a95c11d0ab264f3b1c
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-06 18:25:48 -0600

    📅  | Fri May  6 18:25:48 MDT 2022

---
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  styleUrls: ['./theme-toggle.component.soss'],
+})
+export class ThemeToggleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  toggleTheme(theme: any) {}
+}
